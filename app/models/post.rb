4c8924c8f38645d5d60c35d190ad39938d1c00e9
niclin <bboyceo@hotmail.com>
2018-11-06 22:34:22 +0800

    Add score ranking algorithm

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :votes
 
+  def compute_score!
+    time = (Time.now - self.created_at) / 3600.0
+    gravity = 1.5
+    points = self.votes_count.to_i
 
-
+    score = (points - 1)/(time + 2) ** gravity
+    self.update_attributes(score: score * 1000)
+  end
 end
