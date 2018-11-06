d70d088ccba44da518cdbb343c651974fcaaceb6
niclin <bboyceo@hotmail.com>
2018-11-06 19:12:55 +0800

    Implement user name perfixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,32 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  USERNAME_PATTERN = /\A[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*\z/
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :posts
+
+  validates :name, presence: true, length: { minimum: 5 }, if: :name_changed?, on: :update
+
+  before_validation :create_user_name
+
+  attr_accessor :reset_token
+
+  def create_user_name
+    return if self.name.present?
+
+    email_user_name = self.email.split("@").first.tr(".", "_")
+
+    self.name = valid_user_name?(email_user_name) ? email_user_name : random_user_name
+  end
+
+  private
+
+  def random_user_name
+    "u#{Time.zone.now.to_i}"
+  end
+
+  def valid_user_name?(user_name)
+    USERNAME_PATTERN.match(user_name) && User.where("lower(name) = ?", user_name.downcase).blank?
+  end
 end
